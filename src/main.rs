143bbb57368cdc0d78b1aea938af404728e5f465
BrMtssk <br.mtssk@gmail.com>
2021-03-13 12:55:28 -0300

    [RENAME] targets -> fuzzy_targets
    This aims to make the difference between the 'target' dir
    (automatically created by cargo build) and the fuzzy_targets
    (which are our subjects for fuzzying testing).

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ fn main() {
     let fuzzer = RandomFuzzer { ..RandomFuzzer::default() };
 
     let echo_program_runner = GCovBinaryRunner {
-        program_name: String::from("echo"),
+        binary_path: String::from("fuzzy_targets"),
+        binary_name: String::from("cgi_decode"),
     };
-    let trials = 6;
+    let trials = 3;
     let output = fuzzer.runs(&echo_program_runner, trials);
     println!("{:?}", output)
 }
