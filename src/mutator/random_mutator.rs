67d9f89ea9b64aaca9debb188ae37cacb52c0f7a
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2021-03-26 12:06:21 -0300

    Add new strategy for character generation.

--- src/mutator/random_mutator.rs
+++ src/mutator/random_mutator.rs
@@ -1,20 +1,29 @@
+use std::iter;
 use super::api::Mutator;
-use rand::Rng;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
 
 pub struct RandomMutator {
     pub min_mutations: u32,
     pub max_mutations: u32
 }
 
+enum GenStrategy {
+    Printable,
+    Random,
+}
+
 impl RandomMutator {
 
     fn swap_char(original_string: &String) -> String {
+        let new_character = RandomMutator::generate_random_char(GenStrategy::Random);
+
         if original_string.len() == 0 {
             return original_string.clone();
         }
         let mut rng = rand::thread_rng();
         let mut mutated_string = String::with_capacity(original_string.len());
-        let new_character = rand::random::<char>();
         let position = rng.gen_range(0..original_string.len());
         for (i, c) in original_string.chars().enumerate() {
             if i == position {
@@ -28,7 +37,7 @@ impl RandomMutator {
     }
 
     fn insert_random_character(original_string: &String) -> String {
-        let new_character = rand::random::<char>();
+        let new_character = RandomMutator::generate_random_char(GenStrategy::Random);
         let mut mutated_string = String::with_capacity(original_string.len()+1);
         if original_string.len() == 0 {
             mutated_string.push(new_character);
@@ -64,6 +73,24 @@ impl RandomMutator {
 
         let mutation_index = rand::thread_rng().gen_range(0..mutation_list.len());
         return mutation_list[mutation_index];
+    }
+
+    fn generate_random_char(strategy: GenStrategy) -> char {
+        match strategy {
+            // Generate an alphanumeric character randomly.
+            // References:
+            // - https://docs.rs/rand/0.8.3/rand/distributions/struct.Alphanumeric.html
+            // - https://stackoverflow.com/questions/30811107/how-do-i-get-the-first-character-out-of-a-string
+            GenStrategy::Printable => iter::repeat(())
+                .map(|()| thread_rng().sample(Alphanumeric))
+                .map(char::from)
+                .take(1)
+                .collect::<String>()
+                    .chars()
+                    .nth(0)
+                    .unwrap(),
+            GenStrategy::Random => rand::random::<char>(),
+        }
     }
 }
 
